29739c343972e79b5558c15b4717c9083e4208fa
drh <jmnabangi@gmail.com>
2021-01-06 10:43:28 +0300

    Bug fixes
    Signed-off-by: drh <jmnabangi@gmail.com>

--- prod.sh
+++ prod.sh
@@ -1,5 +1,11 @@
 sudo git pull origin ussd_refactor
 
-sudo chown -R www-data:www-data .
+sudo chown -R $USER:www-data .
 
-sudo chmod -R 777 storage/ bootstrap/cache/
+sudo chmod -R 775 storage/ bootstrap/cache/
+
+sudo supervisorctl reread
+
+sudo supervisorctl update
+
+sudo supervisorctl start laravel-worker:*

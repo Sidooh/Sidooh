7ba522cb6e48529e276f2d6a1380a78e4f4f10ca
drh <jmnabangi@gmail.com>
2021-01-07 21:30:17 +0300

    Added logic for airtime failure
    Signed-off-by: drh <jmnabangi@gmail.com>

--- app/Listeners/AirtimePurchaseFailed.php
+++ app/Listeners/AirtimePurchaseFailed.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\AirtimePurchaseFailedEvent;
+use App\Helpers\AfricasTalking\AfricasTalkingApi;
 use Illuminate\Support\Facades\Log;
 
 class AirtimePurchaseFailed
@@ -27,9 +28,33 @@ class AirtimePurchaseFailed
     public function handle(AirtimePurchaseFailedEvent $event)
     {
         //
-//        TODO:: Send sms notification
-
         Log::info('----------------- Airtime Purchase Failed');
+
+//        TODO: Refund money to voucher
+        $phone = ltrim($event->airtime_response->phoneNumber, '+');
+        $account = $event->airtime_response->request->transaction->account;
+
+        $amount = str_replace(' ', '', explode(".", $event->airtime_response->amount)[0]);
+        $date = $event->airtime_response->updated_at->timezone('Africa/Nairobi')->format(config("settings.sms_date_time_format"));
+
+//        $transaction = new Transaction();
+//
+//        $transaction->amount = $amount;
+//        $transaction->type = 'PAYMENT';
+//        $transaction->description = 'Airtime Refund';
+//        $transaction->account_id = $account->id;
+//        $transaction->product_id = $product->id;
+
+//        $transaction->save();
+
+        $voucher = $account->voucher;
+        $voucher->in += $amount;
+        $voucher->save();
+
+//        TODO:: Send sms notification
+        $message = "Sorry! We could not complete your airtime purchase for {$phone} worth {$amount} on {$date}. We have credited your voucher {$amount} and your balance is now {$voucher->balance}.";
+
+        (new AfricasTalkingApi())->sms($phone, $message);
 
     }
 }

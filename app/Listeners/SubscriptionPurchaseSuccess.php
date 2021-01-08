f18307296fec196c899141c3a0876bcc2bc393df
drh <jmnabangi@gmail.com>
2021-01-08 12:15:52 +0300

    Agent SMS modifications
    Signed-off-by: drh <jmnabangi@gmail.com>

--- app/Listeners/SubscriptionPurchaseSuccess.php
+++ app/Listeners/SubscriptionPurchaseSuccess.php
@@ -38,25 +38,25 @@ class SubscriptionPurchaseSuccess
         $phone = ltrim($account->phone, '+');
 
         $date = $event->subscription->created_at->timezone('Africa/Nairobi')->format(config("settings.sms_date_time_format"));
+        $end_date = $event->subscription->created_at->addMonths($event->subscription->subscription_type->duration)->timezone('Africa/Nairobi')->format(config("settings.sms_date_time_format"));
+
 
         switch ($type->amount) {
             case 4975:
             case 9975:
                 $level_duration = $type->amount == 9975 ? "24 MONTHS" : "18 MONTHS";
-                $message = "Congratulations! You have successfully been pre-registered as a {$type->title} ";
-                $message .= "on {$date}. ";
-                $message .= "You will earn commissions on every airtime purchased by your referred customers ";
-                $message .= "and sub agents UP TO LEVEL {$type->level_limit} FOR {$level_duration} WITHOUT PAYING THE MONTHLY FEES.\n\n";
+                $message = "Congratulations! You have successfully preregistered as a {$type->title} on {$date}, valid until {$end_date}. ";
+                $message .= "You will earn commissions on every airtime purchased by your referred customers and subagents up to your ";
+                $message .= "{$type->level_limit}, for {$level_duration} WITHOUT PAYING MONTHLY SUBSCRIPTION FEES. ";
+                $message .= "Please note, 80% of your commissions will be automatically saved and invested to generate extra income for you.\n";
                 $message .= "Sidooh, Earns you money on every purchase";
 
                 break;
 
             default:
-                $message = "Congratulations! You have successfully subscribed for {$type->title} Services ";
-                $message .= "at Ksh{$type->amount} on {$date}. ";
-                $message .= "This unlocks your referral earnings potential. ";
-                $message .= "You will earn commissions on every purchase by your referred customers ";
-                $message .= "and sub agents up to level {$type->level_limit}.\n\n";
+                $message = "Congratulations! You have successfully registered as a {$type->title} on {$date}, valid until {$end_date}. ";
+                $message .= "You will earn commissions on every airtime purchased by your referred customers and subagents up to your {$type->level_limit}. ";
+                $message .= "Please note, 80% of your commissions will be automatically saved and invested to generate extra income for you.\n";
                 $message .= "Sidooh, Earns you money on every purchase";
         }
 

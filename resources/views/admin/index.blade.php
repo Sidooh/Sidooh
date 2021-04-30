d90edb31308122b341d5a8783960131543632c38
drh <jmnabangi@gmail.com>
2021-05-01 02:27:02 +0300

    Status conformity on blade files
    Signed-off-by: drh <jmnabangi@gmail.com>

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -174,38 +174,44 @@
 
                             <td class="align-middle text-center fs-0 white-space-nowrap payment">
 
-                                @if($transaction->payment->status === 'Complete')
+                                @if(strtolower($transaction->payment->status) === 'complete')
                                     <span class="badge badge rounded-pill badge-soft-success">
                                         <span class="ms-1 fas fa-check" data-fa-transform="shrink-2"></span>
 
-                                @elseif($transaction->payment->status === 'Failed')
-                                            <span class="badge badge rounded-pill badge-soft-secondary">
+                                @elseif(strtolower($transaction->payment->status) === 'failed')
+                                            <span class="badge badge rounded-pill badge-soft-warning">
                                         <span class="ms-1 fas fa-ban" data-fa-transform="shrink-2"></span>
 
+                                @elseif(strtolower($transaction->payment->status) === 'pending')
+                                                    <span class="badge badge rounded-pill badge-soft-primary">
+                                        <span class="ms-1 fas fa-redo" data-fa-transform="shrink-2"></span>
                                 @else
-                                                    <span class="badge badge rounded-pill badge-soft-warning">
+                                                            <span class="badge badge rounded-pill badge-soft-secondary">
                                         <span class="ms-1 fas fa-stream" data-fa-transform="shrink-2"></span>
 
                                 @endif
-                                                        {{ $transaction->payment->status }}
+                                                                {{ $transaction->payment->status }}
                                 </span>
                             </td>
                             <td class="align-middle text-center fs-0 white-space-nowrap payment">
 
-                                @if($transaction->status === 'completed')
+                                @if(strtolower($transaction->status) === 'completed' || strtolower($transaction->status) === 'success')
                                     <span class="badge badge rounded-pill badge-soft-success">
                                         <span class="ms-1 fas fa-check" data-fa-transform="shrink-2"></span>
 
-                                @elseif($transaction->status === 'failed')
-                                            <span class="badge badge rounded-pill badge-soft-secondary">
+                                @elseif(strtolower($transaction->status) === 'failed')
+                                            <span class="badge badge rounded-pill badge-soft-warning">
                                         <span class="ms-1 fas fa-ban" data-fa-transform="shrink-2"></span>
 
+                                @elseif(strtolower($transaction->status) === 'pending')
+                                                    <span class="badge badge rounded-pill badge-soft-primary">
+                                        <span class="ms-1 fas fa-redo" data-fa-transform="shrink-2"></span>
                                 @else
-                                                    <span class="badge badge rounded-pill badge-soft-warning">
+                                                            <span class="badge badge rounded-pill badge-soft-secondary">
                                         <span class="ms-1 fas fa-stream" data-fa-transform="shrink-2"></span>
 
                                 @endif
-                                                        {{ $transaction->status }}
+                                                                {{ $transaction->status }}
                                 </span>
                             </td>
                             <td class="align-middle text-end date">{{ local_date($transaction->updated_at, 'd/m/Y H:i') }}</td>

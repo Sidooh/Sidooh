06dcf2a77b6127a8d9efb10d2f75ada3e0719b4b
drh <jmnabangi@gmail.com>
2022-04-25 22:17:22 +0300

    tries voucher purchase from c2b
    Signed-off-by: drh <jmnabangi@gmail.com>

--- app/Listeners/C2bPaymentConfirmation.php
+++ app/Listeners/C2bPaymentConfirmation.php
@@ -3,6 +3,10 @@
 
 namespace App\Listeners;
 
+use App\Models\Payment;
+use App\Models\Transaction;
+use App\Repositories\AccountRepository;
+use App\Repositories\ProductRepository;
 use DrH\Mpesa\Database\Entities\MpesaStkRequest;
 use DrH\Mpesa\Events\C2bConfirmationEvent;
 use Illuminate\Support\Facades\Log;
@@ -17,12 +21,48 @@ class C2bPaymentConfirmation
      */
     public function handle(C2bConfirmationEvent $event)
     {
-        Log::info(array($event));
+        Log::info($event->mpesa_response);
 
         $c2b = $event->transaction;
         //Try to check if this was from STK
         $request = MpesaStkRequest::whereReference($c2b->BillRefNumber)->first();
 
-        Log::info($request);
+//        Find account
+        $accountRep = new AccountRepository();
+        $account = $accountRep->findByPhone($c2b->MSISDN);
+
+//        Create transaction
+        $productRep = new ProductRepository();
+        $product = $productRep->store(['name' => 'Voucher']);
+
+        $transaction = new Transaction();
+
+        $transaction->amount = $c2b->TransAmount;
+        $transaction->type = 'PAYMENT';
+        $transaction->description = `Voucher Purchase - $c2b->MSISDN`;
+        $transaction->account_id = $account->id;
+        $transaction->product_id = $product->id;
+
+        $transaction->save();
+
+        $payment = new Payment([
+            'amount' => $c2b->TransAmount,
+            'status' => 'Complete',
+            'type' => 'MPESA',
+            'subtype' => 'C2B',
+            'payment_id' => $c2b->id
+        ]);
+
+        $transaction->payment()->save($payment);
+
+//        Purchase Voucher
+        $voucherDetails = [
+            'phone' => $c2b->MSISDN,
+            'amount' => $c2b->TransAmount
+        ];
+
+        (new ProductRepository())->voucher($transaction, $voucherDetails);
+//        Send Notification
+
     }
 }
